109763fdec152758d38ac5b8b7735ec6d72ae822
Arunkumar V S <vsarunkumar27@gmail.com>
2017-12-16 23:54:55 +0530

    first commit

---
+++ config/routes.rb
@@ -0,0 +1,8 @@
+Rails.application.routes.draw do
+  root 'home#index'
+
+  get 'home/about'
+  get 'home/contact'
+
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+end
